df038b2ffef4a13237639a8f07e11c8144c9746a
thankzero <42596740+thankzero@users.noreply.github.com>
2018-08-22 13:29:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require "vendor/autoload.php";
 $access_token = 'lODu4rO6kCgbIcPaV+f1yQfQpHbR3nHIi1nx3Mn/+g9meFbgvAexyZNTaAb52albOgqrX+kvIQMlLgCcut9y692c5VRc17DVMVJqElbEQh6YaBLvexNoaQG2o97Pju2iPhqhc80U79t8DC/Z0xBibwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '82617be1a3978f7a8c802f0844f3124c';
-$idPush = '@bum5144s'
+$idPush = 'immense-ravine-30487'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
