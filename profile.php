3f822a94637391c4e1b6b859c56609d78bb3a9ac
thankzero <42596740+thankzero@users.noreply.github.com>
2018-08-22 11:28:41 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,8 +1,7 @@
 <?php
 
 
-$access_token = 'nCuy0Au7fRsFE+ltZTI7T75MlP+U74Jb1FouPFySMzOAmXxO75Bq6dKDdipyo4rhOgqrX+kvIQMlLgCcut9y692c5VRc17DVMVJqElbEQh6zyv/gJa0qfdkK60VzedFTL7tqKOivu6n+SZRGhoc75QdB04t89/1O/w1cDnyilFU=
-';
+$access_token = 'nCuy0Au7fRsFE+ltZTI7T75MlP+U74Jb1FouPFySMzOAmXxO75Bq6dKDdipyo4rhOgqrX+kvIQMlLgCcut9y692c5VRc17DVMVJqElbEQh6zyv/gJa0qfdkK60VzedFTL7tqKOivu6n+SZRGhoc75QdB04t89/1O/w1cDnyilFU=';
 
 $userId = 'U543d63423fd6d04703170a5d2563a3d5';
 
