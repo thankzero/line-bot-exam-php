afdc907108273e59e1f5e7c1f9a0feba802c994c
thankzero <42596740+thankzero@users.noreply.github.com>
2018-08-22 11:30:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
 <?php
-require "vendor/autoload.php";
-$access_token = 'nCuy0Au7fRsFE+ltZTI7T75MlP+U74Jb1FouPFySMzOAmXxO75Bq6dKDdipyo4rhOgqrX+kvIQMlLgCcut9y692c5VRc17DVMVJqElbEQh6zyv/gJa0qfdkK60VzedFTL7tqKOivu6n+SZRGhoc75QdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '82617be1a3978f7a8c802f0844f3124c';
-$idPush = 'U543d63423fd6d04703170a5d2563a3d5'
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+echo "Hello LINE BOT";
