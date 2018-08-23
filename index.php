2db8c6373683ce3dd9c1543ec7cf6e09c77b1f2a
thankzero <42596740+thankzero@users.noreply.github.com>
2018-08-23 09:32:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require "vendor/autoload.php";
 $access_token = 'QIXKaN9lB/lJ178xbRQRfqo6KuEUr1V0oz/qtTl+OaxHLiC1letsjTvPNffB1RSHw/vHfgfXVOoPCoIx325o+ixa8dnQ7y7bh/XxmVnNkO3mFfQ+ax1VvKY7aDKe0V78tsPWwTNj/Pa4NZOkvWJ6LwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '51d41de8e4556e569c8adb44908b655b';
-$idPush = 'aqueous-chamber-31029.herokuapp.com'
+$idPush = ''
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
